c5643984709e5a2ff2c47a2e7d4caf416fa3c694
James Shawn Carnley <Shawn.Carnley@gatech.edu>
2020-01-28 11:54:07 -0500

    install script tweak

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 #wget -O /var/lib/unbound/root.hints https://www.internic.net/domain/named.root
 
 # debconf-apt-progress seems to hang so get rid of it too
-which debconf-apt-progress
-mv "$(which debconf-apt-progress)" /bin/no_debconf-apt-progress
+#which debconf-apt-progress
+#mv "$(which debconf-apt-progress)" /bin/no_debconf-apt-progress
 
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
 cd "Pi-hole/automated install/"
